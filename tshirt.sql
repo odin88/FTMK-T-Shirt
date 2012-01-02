996c30c3ef95f70db0565b6606b67fa16478f08a
hassanabubakar <odin88@gmail.com>
2012-01-03 01:53:33 +0800

    first commit

---
+++ tshirt.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 02, 2012 at 05:47 PM
+-- Server version: 5.5.13
+-- PHP Version: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `hassanba_tshirt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `no_matric` varchar(30) NOT NULL,
+  `contact_no` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `size_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `name`, `no_matric`, `contact_no`, `email`, `size_id`, `status_id`, `created`, `modified`) VALUES
+(1, 'Hazwan', '1234567', '0123456789', 'hazwan@hazwan.com', 5, 1, '2012-01-02 17:42:54', '2012-01-02 17:42:54');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sizes`
+--
+
+CREATE TABLE IF NOT EXISTS `sizes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `size_name` varchar(10) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `sizes`
+--
+
+INSERT INTO `sizes` (`id`, `size_name`, `created`, `modified`) VALUES
+(1, 'S', '2012-01-02 16:56:36', '2012-01-02 16:56:36'),
+(2, 'M', '2012-01-02 16:56:40', '2012-01-02 16:56:40'),
+(3, 'L', '2012-01-02 16:56:44', '2012-01-02 16:56:44'),
+(4, 'XL', '2012-01-02 16:56:48', '2012-01-02 16:56:48'),
+(5, 'XXL', '2012-01-02 16:56:52', '2012-01-02 16:56:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statuses`
+--
+
+CREATE TABLE IF NOT EXISTS `statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status_name` varchar(50) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `statuses`
+--
+
+INSERT INTO `statuses` (`id`, `status_name`, `created`, `modified`) VALUES
+(1, 'Belum Bayar', '2012-01-02 16:57:33', '2012-01-02 16:57:33'),
+(2, 'Dah Bayar', '2012-01-02 16:57:38', '2012-01-02 16:57:38'),
+(3, 'Belum dapat', '2012-01-02 16:57:48', '2012-01-02 16:57:48'),
+(4, 'Dah dapat', '2012-01-02 16:57:52', '2012-01-02 16:57:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `created`, `modified`) VALUES
+(1, 'admin', 'b7c267f495e615cd6ed30e44abfaeab5ad7e6d54', 'Admin', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
